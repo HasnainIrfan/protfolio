a9a55e47d5da9e81021949465558fbb7889144e9
shahirZain <shahirzain100@gmail.com>
2023-01-08 22:35:10 +0500

    update the repo url

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/ShahirZain/personal-website.git";
+const repoURL = "https://github.com/shahirzian/shahirzian.github.io.git";
 
 ghpages.publish(
   pathname,
